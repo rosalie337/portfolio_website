a14ac48168e940cd025bca7b0a9159baf16897c8
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2020-12-28 21:44:57 -0800

    added website framing components and devData.js

---
+++ src/developerData.js
@@ -0,0 +1,9 @@
+let developerData = {
+    name: 'Rosalie Lee',
+    role: 'Software Engineer',
+    website: 'rosalielee.dev',
+    email: 'inquiry@grosalielee.dev',
+    city: 'Portland, OR'    
+}
+
+export default developerData
