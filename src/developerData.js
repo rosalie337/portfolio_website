01ec3a012adc59fe6163ea4bacd0eac1459af649
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-01-04 19:40:20 -0800

    added more data to developerData.js

--- src/developerData.js
+++ src/developerData.js
@@ -1,9 +1,96 @@
 let developerData = {
     name: 'Rosalie Lee',
-    role: 'Software Engineer',
-    website: 'rosalielee.dev',
+    role: [
+        {
+            title:'Software Engineer',
+            img: 'someurl.com'
+        },
+        {
+            title:'Ballerina',
+            img: 'someurl.com'
+        }, 
+        {
+            title:'Behavior Analyst',
+            img: 'someurl.com'
+        },
+    ],
+    seeking: 'seeking some a Software Engineer role that allows me to work remotely',
     email: 'inquiry@grosalielee.dev',
-    city: 'Portland, OR'    
+    city: 'Portland, OR', 
+    bio:'something something about me',
+    socialMedia: [
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/rosalielee/'
+        },
+        {
+            name: 'Github',
+            url: 'github/rosalie337'
+        },
+    ],
+    portfolio:[
+        {
+            portfolio_img: 'something.img',
+            title: 'some title',
+            description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+            tech: ['some', 'array', 'of', 'tech', 'used'],
+            github_url: 'github.com',
+            site_url: 'site.com',
+        },
+        {    
+            portfolio_img: 'something.img',
+            title: 'some title',
+            description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+            tech: ['some', 'array', 'of', 'tech', 'used'],
+            github_url: 'github.com',
+            site_url: 'site.com',
+        },
+        {   
+            portfolio_img: 'something.img',
+            title: 'some title',
+            description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+            tech: ['some', 'array', 'of', 'tech', 'used'],
+            github_url: 'github.com',
+            site_url: 'site.com',
+        },
+        {   
+            portfolio_img: 'something.img',
+            title: 'some title',
+            description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+            tech: ['some', 'array', 'of', 'tech', 'used'],
+            github_url: 'github.com',
+            site_url: 'site.com',
+        }
+    ],
+    techStack: [
+        {
+            languages: ['Javascript', 'HTML', 'CSS']
+        },
+        {
+            libraries: ['React', 'Express', 'Gatsby']
+        },
+        {
+            framework: ['ReactJS']
+        },
+        {
+            collaboration: ['Slack', 'Jira']
+        },
+        {
+            databases: ['PostgresSQL', 'MongoDB']
+        },
+        {
+            version_control: ['GitHub']
+        },
+        {
+            dev_tools: ['Node JS', 'Heroku', 'Postman', 'Netlify', 'Compass']
+        },
+        {
+            dev_practices: ['TDD', 'BDD']
+        },
+        {
+            emerging_tools: ['React Hooks', 'Redux', 'JSX', 'UX/UI Design']
+        }
+    ]
 }
 
 export default developerData
