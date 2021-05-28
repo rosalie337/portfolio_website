ff9a7660a15a0ba5b38bcc4c740ead265e07ef33
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro-2.local>
2021-05-28 13:52:48 -0700

    added content to Header component, added NavBar and rinstalled related dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import developerData from '../developerData'
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        )
+            <>
+                <header id="home">
+                    <div className="row banner" >
+                        <div className="banner-text">
+                            <h1 className="responsive-headline">
+                                Hi, I'm {developerData.name}.
+                            </h1>
+                            <h3> 
+                                I am a {developerData.role[0].title}.
+                            </h3>
+                            <h4>
+                                I am currently {developerData.seeking}.
+                            </h4>
+                        </div>
+                    </div>
+                </header>
+            </>
+        );
     }
 }
