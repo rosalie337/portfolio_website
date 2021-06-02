af024c3978f326048e8d19ec93be3bdc1e1b9f6c
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro-2.local>
2021-06-02 11:18:14 -0700

    trying to save changes

--- src/components/NavBar/NavBarElements.js
+++ src/components/NavBar/NavBarElements.js
@@ -66,21 +66,3 @@ export const NavBtn = styled.nav
   }
 `;
   
-export const NavBtnLink = styled(Link)
- `border-radius: 4px;
-  background: #808080;
-  padding: 10px 22px;
-  color: #000000;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #808080;
-  }
-`;
